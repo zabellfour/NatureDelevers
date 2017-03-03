611436a537e467092e489782c75ee9af1acee9ee
Dmitry <kostenko.dmitry3@gmail.com>
2017-03-03 23:44:55 +0200

    mobile version

--- src/vendor_entries/vendor.js
+++ src/vendor_entries/vendor.js
@@ -2,5 +2,6 @@ module.exports = [
   './bower/jquery/dist/jquery.js',
   './src/js/modules/main.js',
   './src/js/modules/scrollreveal.min.js',
-  './src/js/modules/tabs.js'
+  './src/js/modules/tabs.js',
+  './src/js/modules/diagram.js'
 ];
