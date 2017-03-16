07051c7e9efc9c54893c7a1cea2a8d23623265a3
zabellfour <zabell2814@gmail.com>
2017-03-16 21:55:41 +0200

    before pull

--- src/js/app.js
+++ src/js/app.js
@@ -2,64 +2,46 @@ import './modules/nav';
 import './modules/tabs';
 import './modules/openClose';
 
-var func1 = fn(
-    function() {
-        $('body').on('click', '.scroll-link', function() {
-            var offset = $('.body-holder').position().top;
-            $(".modal").animate({ scrollTop: offset }, 'slow');
-        });
-    },
-    function() {
-        $(".fancybox-media").fancybox({
-            openEffect: 'none',
-            closeEffect: 'none',
-            helpers: {
-                media: {}
-            }
-        });
-    },
-    function() {
-        $(".economes-popup").fancybox({
-            maxWidth: 500,
-            maxHeight: 400,
-            fitToView: true,
-            autoSize: true,
-            closeClick: false,
-            openEffect: 'none',
-            closeEffect: 'none'
-        });
-    },
-    function() {
-        $(function() {
-            if ($(window).width() > 1024) {
-                skrollr.init({ forceHeight: false });
-                $('.scroll-block').jScrollPane();
-            }
-        });
-    },
-    function() {
-        $(".fancybox-media").fancybox({
-            openEffect: 'none',
-            closeEffect: 'none',
-            helpers: {
-                media: {}
-            }
-        });
-    },
-    function() {
-        $('.scroll-block').jScrollPane();
-
-    }
-);
-
 
 
 
 jQuery(document).ready(function($) {
 
+  
+      $('body').on('click', '.scroll-link', function() {
+        var offset = $('.body-holder').position().top;
+        $(".modal").animate({ scrollTop: offset }, 'slow');
+    });
 
-    func1();
-
-
-
+    $(".fancybox-media").fancybox({
+        openEffect: 'none',
+        closeEffect: 'none',
+        helpers: {
+            media: {}
+        }
+    });
+    $(function() {
+        if ($(window).width() > 1024) {
+            skrollr.init({ forceHeight: false });
+            $('.scroll-block').jScrollPane();
+        }
+    });
+    $(".economes-popup").fancybox({
+        maxWidth: 500,
+        maxHeight: 400,
+        fitToView: true,
+        autoSize: true,
+        closeClick: false,
+        openEffect: 'none',
+        closeEffect: 'none'
+    });
+    $(".fancybox-media").fancybox({
+        openEffect: 'none',
+        closeEffect: 'none',
+        helpers: {
+            media: {}
+        }
+    });
+    $('.scroll-block').jScrollPane();
+    $('.under-mask').jScrollPane();
 });
