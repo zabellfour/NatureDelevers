b7c073c3086f996490cb8fb1b2b2a57f9def21f7
Dmitry <kostenko.dmitry3@gmail.com>
2017-03-18 00:45:12 +0200

    modal elevages desktop

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,17 @@ import './modules/nav';
 import './modules/tabs';
 import './modules/openClose';
 
+
 var initJs = function() {
-    $('div.open-box-text').openClose;
+
+    $('div.open-box-text').openClose({
+        hideOnClickOutside: true,
+        activeClass: 'active',
+        opener: '.open-text-btn',
+        slider: '.holder-text',
+        animSpeed: 400,
+        effect: 'slide'
+    });
     $('body').on('click', '.scroll-link', function() {
         var offset = $('.body-holder').position().top;
         $("body, .modal").animate({ scrollTop: offset }, '.3');
@@ -29,24 +38,15 @@ var initJs = function() {
         openEffect: 'none',
         closeEffect: 'none'
     });
-    $(".fancybox-media").fancybox({
-        openEffect: 'none',
-        closeEffect: 'none',
-        helpers: {
-            media: {}
-        }
-    });
-    skrollr.init({ forceHeight: false });
-    $('#modalChapter').on('shown.bs.modal', function() {
-        skrollr.init().destroy();
-        skrollr.init({ forceHeight: false });
-        initJs();
-    });
-    $('#modalChapter').on('hidden.bs.modal', function() {
-        skrollr.init().destroy();
-    });
+    var skrIn = skrollr.init();
+    skrIn.refresh();
 }
+
 
 jQuery(document).ready(function($) {
     initJs();
+    $('#modalChapter').on('shown.bs.modal', function() {
+        initJs();
+    });
+
 });
