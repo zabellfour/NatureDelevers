6b777bd9fb2cd57dcd35a58601625d4e9344da6d
Dmitry <kostenko.dmitry3@gmail.com>
2017-03-23 20:55:18 +0200

    add js popup plus-btn

--- src/js/journee.js
+++ src/js/journee.js
@@ -2,7 +2,8 @@ $(function(){
 
 	// ---- action slide-right au click sur le bouton "voir plus" en ajoutant de class sliding, en appliquant un effet de transition en css3
 	$('body').on('click', '.btn-plus', function(){
-	 $(this).parents(".container").find(".slide-block").addClass("sliding");
+		$(".slide-block").removeClass("sliding");
+		$(this).parents(".container").find(".slide-block").addClass("sliding");
 	});
 
 	// ---- action clique sur le bloc pour disparaitre le bloc "right-slide-bloc"
